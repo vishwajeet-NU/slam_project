7ed832788feb6b236d8efd626e706a7a663ce5fa
vishwajeet karmarkar <vishwajeetkarmarkar2021@u.northwestern.edu>
2020-01-13 02:27:13 -0600

    all_files_ver1

--- rigid2d/rigid2d.hpp
+++ rigid2d/rigid2d.hpp
@@ -1,13 +1,15 @@
 #ifndef RIGID2D_INCLUDE_GUARD_HPP
 #define RIGID2D_INCLUDE_GUARD_HPP
+#include <math.h>
+#include <iostream>
 /// \file
 /// \brief Library for two-dimensional rigid body transformations.
 
-#include<iosfwd> // contains forward definitions for iostream objects
+//#include<iosfwd> // contains forward definitions for iostream objects
 
 namespace rigid2d
 {
-    /// \brief PI.  Not in C++ standard until C++20.
+ /// \brief PI.  Not in C++ standard until C++20.
     constexpr double PI=3.14159265358979323846;
 
     /// \brief approximately compare two floating-point numbers using
@@ -20,38 +22,35 @@ namespace rigid2d
     /// be useful here
     constexpr bool almost_equal(double d1, double d2, double epsilon=1.0e-12)
     {
+    if(abs(d1-d2) < epsilon)
+    {
+        return(true);
+    }
+    else
+    {
+        return(false);
+    }
+    
     }
 
-    /// \brief convert degrees to radians
-    /// \param deg - angle in degrees
-    /// \returns radians
-    /// NOTE: implement this in the header file
-    /// constexpr means that the function can be computed at compile time
-    /// if given a compile-time constant as input
     constexpr double deg2rad(double deg)
     {
+        return(deg*PI/180);
     }
 
-    /// \brief convert radians to degrees
-    /// \param rad - angle in radians
-    /// \returns the angle in degrees
-    constexpr double rad2deg(double rad)
+     constexpr double rad2deg(double rad)
     {
+        return(rad*180/PI);
     }
 
-    /// static_assertions test compile time assumptions.
-    /// You should write at least one more test for each function
-    /// You should also purposely (and temporarily) make one of these tests fail
-    /// just to see what happens
+
     static_assert(almost_equal(0, 0), "is_zero failed");
     static_assert(almost_equal(0.001, 0.005, 1.0e-2), "is_zero failed");
-
     static_assert(almost_equal(deg2rad(0.0), 0.0), "deg2rad failed");
 
     static_assert(almost_equal(rad2deg(0.0), 0.0), "rad2deg) failed");
 
     static_assert(almost_equal(deg2rad(rad2deg(2.1)), 2.1), "deg2rad failed");
-
 
     /// \brief A 2-Dimensional Vector
     struct Vector2D
@@ -60,81 +59,61 @@ namespace rigid2d
         double y = 0.0;
     };
 
+    class Twist2D
+    {
+        public: 
+
+        double w = 0.0;
+        double v_x = 0.0;
+        double v_y = 0.0;
+        float z1,z2,z5,z6;
+        float z3,z4,z7,z8=0;
+        float z9,z10,z11,z12,z13,z14,z15,z16;
+        void Twist2D::Adjoint(Transform2D & adtf);
+
+    };
+
     /// \brief output a 2 dimensional vector as [xcomponent ycomponent]
     /// os - stream to output to
     /// v - the vector to print
+
     std::ostream & operator<<(std::ostream & os, const Vector2D & v);
 
-    /// \brief input a 2 dimensional vector
-    ///   You should be able to read vectors entered as two numbers
-    ///   separated by a newline or a space, or entered as [xcomponent ycomponent]
-    /// is - stream from which to read
-    /// v [out] - output vector
-    /// Hint: The following may be useful:
-    /// https://en.cppreference.com/w/cpp/io/basic_istream/peek
-    /// https://en.cppreference.com/w/cpp/io/basic_istream/get
+
     std::istream & operator>>(std::istream & is, Vector2D & v);
 
-    /// \brief a rigid body transformation in 2 dimensions
-    class Transform2D
+
+
+ class Transform2D
     {
     public:
-        /// \brief Create an identity transformation
-        Transform2D();
-
-        /// \brief create a transformation that is a pure translation
-        /// \param trans - the vector by which to translate
-        explicit Transform2D(const Vector2D & trans);
-
-        /// \brief create a pure rotation
-        /// \param radians - angle of the rotation, in radians
-        explicit Transform2D(double radians);
-
-        /// \brief Create a transformation with a translational and rotational
-        /// component
-        /// \param trans - the translation
-        /// \param rot - the rotation, in radians
-        Transform2D(const Vector2D & trans, double radians);
-
-        /// \brief apply a transformation to a Vector2D
-        /// \param v - the vector to transform
-        /// \return a vector in the new coordinate system
-        Vector2D operator()(Vector2D v) const;
-
-        /// \brief invert the transformation
-        /// \return the inverse transformation. 
-        Transform2D inv() const;
-
-        /// \brief compose this transform with another and store the result 
-        /// in this object
-        /// \param rhs - the first transform to apply
-        /// \returns a reference to the newly transformed operator
-        Transform2D & operator*=(const Transform2D & rhs);
-
-        /// \brief \see operator<<(...) (declared outside this class)
-        /// for a description
-        friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
-    };
+           
+    float m1,m2,m3,m4,m5,m6;
+    float m7= 0;
+    float m8 =0;
+    float m9 =1;
 
 
-    /// \brief should print a human readable version of the transform:
-    /// An example output:
-    /// dtheta (degrees): 90 dx: 3 dy: 5
-    /// \param os - an output stream
-    /// \param tf - the transform to print
+    Transform2D();
+    explicit Transform2D(const Vector2D & trans);    
+    explicit Transform2D(double radians);
+    Transform2D(const Vector2D & trans, double radians);
+
+
+    Vector2D operator()(Vector2D v) const;
+    Transform2D inv() const;
+    Transform2D & operator*=(const Transform2D & rhs);
+    friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+     
+     };
+     
     std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
-
-    /// \brief Read a transformation from stdin
-    /// Should be able to read input either as output by operator<< or
-    /// as 3 numbers (degrees, dx, dy) separated by spaces or newlines
     std::istream & operator>>(std::istream & is, Transform2D & tf);
-
-    /// \brief multiply two transforms together, returning their composition
-    /// \param lhs - the left hand operand
-    /// \param rhs - the right hand operand
-    /// \return the composition of the two transforms
-    /// HINT: This function can be implemented in terms of *=
     Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
+
+
+
 }
+
 
 #endif
--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -5,22 +5,59 @@
 #include <sstream>
 #include <turtlesim/TeleportAbsolute.h>
 #include <turtlesim/SetPen.h>
+#include<std_srvs/Empty.h>
+#include <turtlesim/Pose.h> 
+#include "tsim/PoseError.h"
 
 
 geometry_msgs::Twist msg;
+tsim::PoseError er;
+turtlesim::Pose pose;
+
+bool up= true;
+bool right = true;
+namespace turtle_pose{
+    float x = 0.0;
+    float y = 0.0;
+    float theta = 0.0;
+}
+
+void poseCallback(const turtlesim::Pose &pose){
+   turtle_pose::x = pose.x;
+   turtle_pose::y = pose.y;
+   turtle_pose::theta = pose.theta;
+}
+
+ 
 
 
 int main(int argc, char **argv)
 {
+int width;
+int height;
+int trans_vel ;
+int rot_vel;
+int abs_x ;
+int abs_y;
+
 
 ros::init(argc, argv, "turtle_rect");
 ros::NodeHandle n;
 
-ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+n.getParam("width",width);
+n.getParam("height",height);
+n.getParam("trans_vel",trans_vel);
+n.getParam("rot_vel",rot_vel);
+n.getParam("abs_x",abs_x);
+n.getParam("abs_y",abs_y);
+
+
+ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 100);
+ros::Publisher err_pub = n.advertise<tsim::PoseError>("turtle1/pose_err", 100);
+ros::Subscriber sub = n.subscribe("turtle1/pose", 1, poseCallback);
+ros::Rate rate(1);
 
 int local_a;
-
-ros::Rate rate(1);
 n.getParam("param2",local_a);
 
 ros::service::waitForService("spawn");
@@ -35,24 +72,217 @@ srv2.request.width =1;
 srv2.request.off =1;
 
 client2.call(srv2);
-
-
 ros::ServiceClient client = n.serviceClient<turtlesim::TeleportAbsolute>("turtle1/teleport_absolute");
 
 turtlesim::TeleportAbsolute srv;
-srv.request.x =5;
-srv.request.y =5;
+srv.request.x =abs_x;
+srv.request.y =abs_y;
 srv.request.theta =0;
 client.call(srv);
 
 srv2.request.off =0;
 client2.call(srv2);
 
+
+
+
+while (ros::ok())
+{
+
+float x_error;
+float y_error;
+int which_part = 1; // think where this should be properly defined.. maybe it should come in at reset?
+
+
+// going right 
+
+ros::Time start = ros::Time::now();
+while(ros::Time::now() - start < ros::Duration(2.0)){
+msg.linear.x = trans_vel;
+msg.angular.z = 0;
+vel_pub.publish(msg);
+
+ros::Duration inner_time1 = ros::Time::now() - start;
+ros::spinOnce();
+
+float secs;
+secs = inner_time1.toSec(); 
+x_error = turtle_pose::x - (secs*trans_vel + abs_x);
+y_error = turtle_pose::y - abs_y;
+//ROS_INFO_STREAM(x_error);
+//ROS_INFO_STREAM(y_error);
+er.x_error = x_error;
+er.y_error = y_error;
+er.theta_error = 0;
+err_pub.publish(er);
+
+}
+ros::Time start2 = ros::Time::now();
+
+while(ros::Time::now() - start2 < ros::Duration(1.57079)){
+
+msg.linear.x = 0;
+msg.angular.z = rot_vel;
+vel_pub.publish(msg);
+
+}
+
+// going up 
+ros::Time start1 = ros::Time::now();
+while(ros::Time::now() - start1 < ros::Duration(2.5)){
+msg.linear.x = trans_vel;
+msg.angular.z = 0;
+vel_pub.publish(msg);
+
+ros::Duration inner_time2 = ros::Time::now() - start1;
+ros::spinOnce();
+
+float secs2;
+secs2 = inner_time2.toSec(); 
+x_error = turtle_pose::x - (abs_x+width) ;
+y_error = turtle_pose::y - (secs2*trans_vel + abs_y);
+//ROS_INFO_STREAM(x_error);
+//ROS_INFO_STREAM(y_error);
+
+er.x_error = x_error;
+er.y_error = y_error;
+er.theta_error = 0;
+err_pub.publish(er);
+
+}
+ros::Time start4 = ros::Time::now();
+
+while(ros::Time::now() - start4 < ros::Duration(1.57079)){
+
+msg.linear.x = 0;
+msg.angular.z = rot_vel;
+vel_pub.publish(msg);
+
+}
+
+ros::Time start5 = ros::Time::now();
+while(ros::Time::now() - start5 < ros::Duration(2.0)){
+msg.linear.x = trans_vel;
+msg.angular.z = 0;
+vel_pub.publish(msg);
+
+ros::Duration inner_time5 = ros::Time::now() - start5;
+ros::spinOnce();
+
+float secs5;
+secs5 = inner_time5.toSec(); 
+x_error = turtle_pose::x - (abs_x + width - secs5*trans_vel );
+y_error = turtle_pose::y - (abs_y+height);
+//ROS_INFO_STREAM(x_error);
+//ROS_INFO_STREAM(y_error);
+er.x_error = x_error;
+er.y_error = y_error;
+er.theta_error = 0;
+err_pub.publish(er);
+
+
+}
+
+ros::Time start6 = ros::Time::now();
+
+while(ros::Time::now() - start6 < ros::Duration(1.57079)){
+
+msg.linear.x = 0;
+msg.angular.z = rot_vel;
+vel_pub.publish(msg);
+
+}
+ros::Time start7 = ros::Time::now();
+while(ros::Time::now() - start7 < ros::Duration(2.5)){
+msg.linear.x = trans_vel;
+msg.angular.z = 0;
+vel_pub.publish(msg);
+
+ros::Duration inner_time7 = ros::Time::now() - start7;
+ros::spinOnce();
+
+float secs7;
+secs7 = inner_time7.toSec(); 
+x_error = turtle_pose::x - (abs_x) ;
+y_error = turtle_pose::y - (abs_y+ height - secs7*trans_vel );
+//ROS_INFO_STREAM(x_error);
+//ROS_INFO_STREAM(y_error);
+er.x_error = x_error;
+er.y_error = y_error;
+er.theta_error = 0;
+err_pub.publish(er);
+
+
+}
+
+ros::Time start8 = ros::Time::now();
+
+while(ros::Time::now() - start8 < ros::Duration(1.57079)){
+
+msg.linear.x = 0;
+msg.angular.z = rot_vel;
+vel_pub.publish(msg);
+
+}
+
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+ros::Time start2 = ros::Time::now();
+
+while(ros::Time::now() - start2 < ros::Duration(5.0)){
+
+msg.linear.x = 0;
+msg.angular.z = 0.314;
+vel_pub.publish(msg);
+
+}
+
+ros::Time start3 = ros::Time::now();
+
+while(ros::Time::now() - start3 < ros::Duration(5.0)){
+
+msg.linear.x = 1;
+msg.angular.z = 0;
+vel_pub.publish(msg);
+
+}
+ros::Time start4 = ros::Time::now();
+
+while(ros::Time::now() - start4 < ros::Duration(5.0)){
+
+msg.linear.x = 0;
+msg.angular.z = 0.314;
+vel_pub.publish(msg);
+
+}
+
 while (ros::ok())
 {
 ros::service::waitForService("spawn");
 
-/*
 
 distance = speed * time 
 speed = known. 
@@ -60,7 +290,6 @@ start position = known.
 
 
 
-*/
 
 
 ros::Time start = ros::Time::now();
@@ -122,5 +351,3 @@ ROS_INFO("%s", msg.data.c_str());
 ++count;
 }
 */
-
-}    
